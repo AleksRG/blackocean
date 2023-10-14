f403cf2f5fb977ecfd4328fb2ea8f737f795e9a9
AleksRG <aleksrdev@gmail.com>
2023-10-14 23:45:04 +0300

    update connect btn

--- components/header.tsx
+++ components/header.tsx
@@ -8,8 +8,11 @@ export function Header() {
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center w-full mx-auto max-w-7xl justify-between space-x-2">
         <IconBC />
-        <LoginButton />
-        <ThemeToggle />
+
+        <div className="flex items-center justify-center">
+          <LoginButton />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   )
--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,66 +1,44 @@
 'use client'
-
+import { LockClosedIcon } from '@radix-ui/react-icons'
 import * as React from 'react'
 import { useWallet } from '@solana/wallet-adapter-react'
 import dynamic from 'next/dynamic'
-const WalletMultiButtonDynamic = dynamic(
-  async () =>
-    (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
-  { ssr: false }
-)
-
-const WalletDisconnectButtonDynamic = dynamic(
-  async () =>
-    (await import('@solana/wallet-adapter-react-ui')).WalletDisconnectButton,
-  { ssr: false }
-)
-
-import { type ButtonProps } from '@/components/ui/button'
+import { IconUser } from '@/components/ui/icons'
+import { Button, type ButtonProps } from '@/components/ui/button'
 
 interface LoginButtonProps extends ButtonProps {
   showGithubIcon?: boolean
   text?: string
 }
-
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+)
 export function LoginButton({
   text = 'Login with Wallet',
   showGithubIcon = true,
   className,
   ...props
 }: LoginButtonProps) {
-  const { publicKey } = useWallet()
+  const { publicKey, disconnect, connecting } = useWallet()
 
   return (
-    // <Button className={cn(className)} {...props} variant="secondary">
-    <>
-      {!publicKey ? (
-        <WalletMultiButtonDynamic
-          style={{
-            fontSize: 15,
-            fontWeight: 'normal',
-            background: 'transparent',
-            height: 0
-          }}
-        >
-          <span className="px-2 text-center text-xs leading-normal text-muted-foreground ">
-            Connect
-          </span>
-        </WalletMultiButtonDynamic>
+    <div className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 shadow-none hover:bg-accent hover:text-accent-foreground w-9 h-8">
+      {!publicKey && !connecting ? (
+        <>
+          <LockClosedIcon className="transition-all absolute" />
+          <WalletMultiButtonDynamic
+            style={{
+              background: 'transparent'
+            }}
+          >
+            {' '}
+          </WalletMultiButtonDynamic>
+        </>
       ) : (
-        <WalletDisconnectButtonDynamic
-          style={{
-            fontSize: 15,
-            fontWeight: 'normal',
-            background: 'transparent'
-          }}
-        >
-          {' '}
-          <span className="px-2 text-center text-xs leading-normal text-muted-foreground ">
-            Disconnect
-          </span>
-        </WalletDisconnectButtonDynamic>
-      )}{' '}
-    </>
-    // </Button>
+        <IconUser className="transition-all absolute" onClick={disconnect} />
+      )}
+    </div>
   )
 }
